06d62498fb88eb9aabcf8da81d5667fe6df9d550
Daykair <daykair1@gmail.com>
2025-03-15 00:00:46 -0400

    Adding pluginh upload

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+export default () => ({upload: {
+  config: {
+    provider: '@strapi/upload',
+    providerOptions: {
+      // Configuración de tu proveedor (ej: AWS S3, Cloudinary, etc.)
+    },
+  },
+},});
