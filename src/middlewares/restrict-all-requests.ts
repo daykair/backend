8a92219b17f974553c61499d30c8004e55f59a06
Daykair <daykair1@gmail.com>
2025-07-02 13:52:35 -0400

    update allowed origins and IPs in request restriction middleware

--- src/middlewares/restrict-all-requests.ts
+++ src/middlewares/restrict-all-requests.ts
@@ -1,20 +1,21 @@
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
-    const allowedOrigins = ['https://front-saodi.vercel.app', 'http://localhost:3000'];
-    const allowedIPs = ['100.64.0.5', '100.64.0.2', '100.64.0.3', '100.64.0.4', '100.64.0.6', '100.64.0.7', '100.64.0.8', '100.64.0.9'];
+    const allowedOrigins = ['https://front-saodi.vercel.app', 'http://localhost:3000', 'https://saodi.store/', 'https://www.saodi.store/'];
+    const allowedIPs = ['100.64.0.5', '100.64.0.2', '100.64.0.3', '100.64.0.4', '100.64.0.6', '100.64.0.7', '100.64.0.8', '100.64.0.9', '127.0.0.1'];
 
     // Obtener IP y Origen/Referer del cliente
     const clientIP = ctx.request.ip;
     const origin = ctx.request.headers.origin || ctx.request.headers.referer;
-    console.log('Client Origin:', origin);
-    console.log('Client IP:', clientIP);
+    console.log('Client Origin:', origin, 'Client IP:', clientIP);
+    
     // Validar si coincide con el origen o IP permitidos
     const isValidOrigin = allowedOrigins.some((allowedOrigin) => allowedOrigin.startsWith(origin));
     const isValidIP = allowedIPs.includes(clientIP);
-  
+    
+    console.log('isValidOrigin:', isValidOrigin, 'isValidIP:', isValidIP);
 
     // Bloquear si no cumple
-    if (!isValidOrigin && !isValidIP) {
+    if (!isValidOrigin || !isValidIP) {
       return ctx.unauthorized('Acceso no autorizado.');
     }
 
