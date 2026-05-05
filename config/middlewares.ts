d5c27f09f903c862269ad6cb0896373a17261c7b
Daykair <daykair1@gmail.com>
2026-05-04 21:56:15 -0400

    feat: configure AWS-S3 upload provider for Backblaze B2 integration in Strapi middleware and plugins

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
 	"strapi::logger",
 	"strapi::errors",
-	"strapi::security",
+	{
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.backblazeb2.com', // Autoriza las imágenes alojadas en B2
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.backblazeb2.com', // Autoriza videos/audios alojados en B2
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 	"strapi::cors",
 	{ name: "strapi::poweredBy", config: { PoweredBy: "Saodi Development" } },
 	"strapi::query",
