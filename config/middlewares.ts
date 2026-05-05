377bd04c691459d98b6dba75f37b81958f0cc0fc
daykair <daykair@users.noreply.github.com>
2026-05-05 12:12:46 -0400

    feat: configure Backblaze B2 S3 storage provider and update security middleware policies

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,35 @@
 export default [
 	"strapi::logger",
 	"strapi::errors",
-	"strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "s3.us-east-005.backblazeb2.com",
+            "saodi-strapi.s3.us-east-005.backblazeb2.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "s3.us-east-005.backblazeb2.com",
+            "saodi-strapi.s3.us-east-005.backblazeb2.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
 	"strapi::cors",
 	{ name: "strapi::poweredBy", config: { PoweredBy: "Saodi Development" } },
 	"strapi::query",
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,27 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('B2_KEY_ID'),
+            secretAccessKey: env('B2_APP_KEY'),
+          },
+          region: env('B2_REGION'),
+          endpoint: env('B2_ENDPOINT'),
+          forcePathStyle: true,
+          params: {
+            Bucket: env('B2_BUCKET_NAME'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
+
